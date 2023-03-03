2e639ca4ec5d6b9051aa33b738b50c2748cb92ac
prakash <ch.saiprakash22@gmail.com>
2023-03-03 16:55:30 -0700

    read the backendurl from .env

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,6 @@
 // console.log(process.env.REACT_APP_BACKEND_URL)
+<<<<<<< HEAD
 export const backend_url = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:5000"
+=======
+export const backend_url = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:5000"
+>>>>>>> 6c91605 (read the backendurl from .env)
