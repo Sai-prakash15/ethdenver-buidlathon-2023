899bcc44a4707d52a7cd88724809ea1c929811a2
prakash <ch.saiprakash22@gmail.com>
2023-03-04 15:55:31 -0700

    Add feedback

--- frontend/src/Components/input/feedback.js
+++ frontend/src/Components/input/feedback.js
@@ -1,26 +1,62 @@
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import "./feedback.css";
 
 function Feedback(props) {
+
+    const [activeBtn, setActiveBtn] = useState("none");
+    const {metamask_connected} = props;
+
+    const handleLikeClick = () => {
+        if (activeBtn === "none") {
+          setActiveBtn("like");
+          return;
+        }
+     
+        if (activeBtn === 'like'){
+          setActiveBtn("none");
+          return;
+        }
+     
+        if (activeBtn === "dislike") {
+          setActiveBtn("like");
+        }
+      };
+      const handleDisikeClick = () => {
+        if (activeBtn === "none") {
+          
+          setActiveBtn("dislike");
+          return;
+        }
+       
+        if (activeBtn === 'dislike'){
+          setActiveBtn("none");
+          return;
+        }
+     
+        if (activeBtn === "like") {
+          setActiveBtn("dislike");
+        }
+      };
    
-  return (
-    <>
-    <ThumbUpIcon/>
-    <ThumbDownIcon/>
-    </>
-      )
+  return  metamask_connected && (
+    <Box sx={{ display: 'inline-flex' }}>
+    <Typography variant="p" sx={{color:"white", fontSize:"25px", marginTop:"18px"}}>Expected Result ?</Typography>
+    <ThumbUpIcon className={`btn ${activeBtn === "like" ? "like-active" : ""}`} sx={{marginLeft:"10px"}} onClick={handleLikeClick}/>
+    <ThumbDownIcon className={`btn ${activeBtn === "dislike" ? "dislike-active" : ""}`} onClick={handleDisikeClick}/>
+    </Box>
+    )
+      
 }
 
-// const mapStateToProps = state => {
-//     return {
-//       visualization: state.counter.visualization,
-//     }
-//   }
-
-//   const mapDispatchToProps = dispatch => {
-//     return {
-//         setVisualization: (data) => dispatch(setVisualization(data))
-//     }
-//   }
-
-export default Feedback;
+const mapStateToProps = state => {
+    return {
+      metamask_connected: state.counter.metamask_connected,
+    }
+  }
+  
+export default connect(mapStateToProps)(Feedback);
