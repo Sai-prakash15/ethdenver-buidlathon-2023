28f7a8876b32d7508bb0060cd12e87d11e2e1c60
gosuto-inzasheru <mail@gosuto.ai>
2023-03-05 00:23:28 -0700

    chore: clean up stdout

--- backend/backend/services/openAI/service.py
+++ backend/backend/services/openAI/service.py
@@ -2,7 +2,12 @@ import openai
 
 from backend.config import OPENAI_API_KEY
 from backend.services.openAI.graph_prompt_factory import GraphPromptFactory
-from llama_index import LLMPredictor, GPTSimpleVectorIndex, SimpleDirectoryReader, PromptHelper
+from llama_index import (
+    LLMPredictor,
+    GPTSimpleVectorIndex,
+    SimpleDirectoryReader,
+    PromptHelper,
+)
 from llama_index.indices import GPTListIndex
 from llama_index import Document
 from backend.services.graph.service import GraphService
@@ -12,10 +17,7 @@ from langchain import OpenAI
 from backend.services.graph import subgraphs
 import os
 
-PROTOCOL_TO_PATH = {
-    "opensea-v2": "opensea",
-    "opensea-v1": "opensea"
-}
+PROTOCOL_TO_PATH = {"opensea-v2": "opensea", "opensea-v1": "opensea"}
 
 
 def protocol_path_formatter(protocol):
@@ -34,11 +36,15 @@ class OpenAIService:
     def request_gql_for_graph_llama(self, input_query, subgraph):
         # import regex as re
         graph_service = GraphService(protocol=subgraph)
-        schema = os.path.join(os.getcwdb().decode("utf-8"), graph_service.subgraph.schema_file_location)
-        mappings = mapping_path(graph_service.subgraph.deployments['base'])
+        schema = os.path.join(
+            os.getcwdb().decode("utf-8"), graph_service.subgraph.schema_file_location
+        )
+        mappings = mapping_path(graph_service.subgraph.deployments["base"])
         # examples = os.getcwdb().decode("utf-8")+ "/backend/services/graph/graphql_examples.py"
         # set recursive = True for case of uniswap etc where there are more sub directories
-        documents = SimpleDirectoryReader(input_dir=mappings, input_files=[schema], recursive=True).load_data()
+        documents = SimpleDirectoryReader(
+            input_dir=mappings, input_files=[schema], recursive=True
+        ).load_data()
         # print("documents", documents)
         # save to disk
         # index.save_to_disk('index.json')
@@ -46,7 +52,10 @@ class OpenAIService:
         # index = GPTSimpleVectorIndex.load_from_disk('index.json')
 
         # define LLM
-        llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+        print("==========openai training:==========")
+        llm_predictor = LLMPredictor(
+            llm=OpenAI(temperature=0, model_name="text-davinci-003")
+        )
 
         # define prompt helper
         # set maximum input size
@@ -61,10 +70,14 @@ class OpenAIService:
             documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
         )
 
-
         prompt = GraphPromptFactory(subgraph).build_prompt_for_subgraph(input_query)
         response = index.query(prompt)
 
         openai_result = response.response
         print("==========openai response:==========\n", openai_result)
+        # strip any unnecessary text prepended and/or postpended to the gql query
+        openai_result = openai_result[
+            openai_result.find("{") : openai_result.rfind("}") + 1
+        ]
+        print("==========openai response (formatted):==========\n", openai_result)
         return openai_result
