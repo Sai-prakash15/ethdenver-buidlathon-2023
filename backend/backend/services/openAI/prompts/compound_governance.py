822850402d80375186df3842974ecfb1e30f10ea
marissaposner <marissaposner14@gmail.com>
2023-03-05 08:40:28 -0500

    fix: sort in correct direction

--- backend/backend/services/openAI/prompts/compound_governance.py
+++ backend/backend/services/openAI/prompts/compound_governance.py
@@ -61,7 +61,7 @@ o="""{
     where: {proposal_: {id: "86"}
     }
     orderBy: timestamp
-    orderDirection: desc
+    orderDirection: asc
   ) {
     abstainWeightedVotes
     againstWeightedVotes
