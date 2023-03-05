2097cb79ff1a190e1ade005ae37886fb86412876
marissaposner <marissaposner14@gmail.com>
2023-03-05 02:52:43 -0500

    fix: merge conflict

--- backend/backend/services/openAI/prompts/aave_governance.py
+++ backend/backend/services/openAI/prompts/aave_governance.py
@@ -2,9 +2,6 @@ from backend.services.openAI.graph_prompt import GraphPromptBase as Prompt
 
 
 EXAMPLES = [
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-
 Prompt(
 q="What is the aave proposal with the most votes?",
 o="""query MostVotedProposal {
@@ -21,34 +18,6 @@ o="""query {
     againstWeightedVotes
   }
 }"""),
-
-
-=======
-=======
->>>>>>> 37d48116677ba55d9adcd4e23064a1069d15944a
-    Prompt(
-        q="What is the aave proposal with the most votes?",
-        o="""{
-          proposals(orderBy: totalWeightedVotes, orderDirection: desc, first: 1) {
-            id
-            totalWeightedVotes
-          }
-<<<<<<< HEAD
-        }"""
-=======
-        }""",
->>>>>>> 37d48116677ba55d9adcd4e23064a1069d15944a
-    ),
-    Prompt(
-        q="Count the votes for and against the last 10 aave proposals",
-        o="""{
-          proposals(orderBy: creationTime, orderDirection: desc, first: 10) {
-            forWeightedVotes
-            againstWeightedVotes
-          }
-        }""",
-    ),
-<<<<<<< HEAD
 Prompt(
 q="How many votes did proposal 86 have?",
 o="""query{
@@ -58,7 +27,7 @@ o="""query{
 }"""),
 
 Prompt(
-q="Query: What was the voting timeline for for, against, and abstain votes for proposal 86?",
+q="Query: What was the voting timeline for Against, For, and Abstain votes for proposal 86?",
 o="""query {
   voteDailySnapshots(
     where: {proposal_: {id: "86"}
@@ -76,10 +45,4 @@ o="""query {
     }
   }
 }""")
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> 37d48116677ba55d9adcd4e23064a1069d15944a
 ]
